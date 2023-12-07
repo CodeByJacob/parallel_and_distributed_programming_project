cebf051ccc93cd8759d9e1a7a11304814c38326e
pploszczyca <piotrus.plo@gmail.com>
2023-12-07 12:09:28 +0100

    Prepared MPI AES decrypt/encrypt implementation

--- app/aes/aes_mpi.c
+++ app/aes/aes_mpi.c
@@ -1,2 +1,34 @@
 #include "aes_mpi.h"
+#include <mpi.h>
 
+#define BLOCK_SIZE 16
+
+void aesEncrypt(uint8_t *inputData, uint8_t *outputData, uint8_t *roundKeys) {
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    uint8_t localInputBlock[BLOCK_SIZE];
+    uint8_t localOutputBlock[BLOCK_SIZE];
+
+    MPI_Scatter(inputData, BLOCK_SIZE, MPI_UINT8_T, localInputBlock, BLOCK_SIZE, MPI_UINT8_T, 0, MPI_COMM_WORLD);
+
+    aesEncrypt(localInputBlock, localOutputBlock, roundKeys);
+
+    MPI_Gather(localOutputBlock, BLOCK_SIZE, MPI_UINT8_T, outputData, BLOCK_SIZE, MPI_UINT8_T, 0, MPI_COMM_WORLD);
+}
+
+void aesDecrypt(uint8_t *inputData, uint8_t *outputData, uint8_t *roundKeys) {
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    uint8_t localInputBlock[BLOCK_SIZE];
+    uint8_t localOutputBlock[BLOCK_SIZE];
+
+    MPI_Scatter(inputData, BLOCK_SIZE, MPI_UINT8_T, localInputBlock, BLOCK_SIZE, MPI_UINT8_T, 0, MPI_COMM_WORLD);
+
+    aesDecrypt(localInputBlock, localOutputBlock, roundKeys);
+
+    MPI_Gather(localOutputBlock, BLOCK_SIZE, MPI_UINT8_T, outputData, BLOCK_SIZE, MPI_UINT8_T, 0, MPI_COMM_WORLD);
+}
--- app/aes/aes_mpi.h
+++ app/aes/aes_mpi.h
@@ -3,22 +3,6 @@
 
 #include "aes_common.h"
 
-void addRoundKey(uint8_t *state, uint8_t *roundKeyMatrix, uint8_t roundNumber);
-
-void mixColumns(uint8_t *state);
-
-void invMixColumns(uint8_t *state);
-
-void shiftRows(uint8_t *state);
-
-void invShiftRows(uint8_t *state);
-
-void subBytes(uint8_t *state);
-
-void invSubBytes(uint8_t *state);
-
-void keyExpansion(uint8_t *originalKey, uint8_t *expandedKey);
-
 void aesEncrypt(uint8_t *inputBlock, uint8_t *outputBlock, uint8_t *roundKeys);
 
 void aesDecrypt(uint8_t *inputBlock, uint8_t *outputBlock, uint8_t *roundKeys);

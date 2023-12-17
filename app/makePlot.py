a8c9505361f4d7bae0f2da17a41a287effb65922
pploszczyca <piotrus.plo@gmail.com>
2023-12-17 12:31:53 +0100

    Adjusted ploting script

--- app/makePlot.py
+++ app/makePlot.py
@@ -25,7 +25,8 @@ def extract_data_from_logs(logs):
     for log in logs:
         parts = log.split("|")
         test_category, test_name, size, clock_time = [part.split(":")[1] for part in parts]
-        data[test_name][(test_category, int(size))].append(float(clock_time))
+        # Convert seconds to milliseconds
+        data[test_name][(test_category, int(size))].append(float(clock_time) * 1000)
     return data
 
 
@@ -34,7 +35,7 @@ def make_plot(data, results_folder):
         fig, ax = plt.subplots(figsize=(10, 5))
         plt.title(f'Test: {test_name}')
         ax.set_xlabel("Category - Size", fontsize=12)
-        ax.set_ylabel("Average Time (seconds)", fontsize=12)
+        ax.set_ylabel("Average Time (milliseconds)", fontsize=12)
 
         unique_sizes = set(size for _, size in categories.keys())
         unique_categories = sorted(set(category for category, _ in categories.keys()))
@@ -44,18 +45,21 @@ def make_plot(data, results_folder):
         colors = plt.cm.viridis(np.linspace(0, 1, len(unique_sizes)))
 
         for i, size in enumerate(sorted(unique_sizes)):
-            times = [sum(categories[(category, size)])/len(categories[(category, size)]) if (category, size) in categories else 0 for category in unique_categories]
-            rects = ax.bar([x + i * bar_width for x in x_base], times, width=bar_width, label=f'Size {size}', color=colors[i])
+            times = [sum(categories[(category, size)]) / len(categories[(category, size)]) if (category,
+                                                                                               size) in categories else 0
+                     for category in unique_categories]
+            rects = ax.bar([x + i * bar_width for x in x_base], times, width=bar_width, label=f'Size {size}',
+                           color=colors[i])
 
             for rect in rects:
                 height = rect.get_height()
-                ax.annotate(f'{height:.2e}',
+                ax.annotate(f'{height:.2f}',
                             xy=(rect.get_x() + rect.get_width() / 2, height),
                             xytext=(0, 3),  # 3 points vertical offset
                             textcoords="offset points",
                             ha='center', va='bottom', fontsize=8)
 
-        ax.set_xticks([x + bar_width for x in x_base])
+        ax.set_xticks([x + bar_width * (len(unique_sizes) - 1) / 2 for x in x_base])
         ax.set_xticklabels(unique_categories, rotation=45, ha="right", fontsize=10)
 
         ax.legend(title="Size", fontsize='small', loc='upper left')
